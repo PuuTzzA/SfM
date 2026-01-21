86976ee0595ae0bfaa7c68eb2ab8bd7c478e1628
mulsicpp <christianmulser000@gmail.com>
2026-01-21 14:11:11 +0000

    added CameraCalibration struct

---
+++ submodules/calibrate/calibrate.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "../SfM"
+
+#include <vector>
+#include <opencv2/opencv.hpp>
+
+namespace SfM::calibrate {
+
+    struct CameraCalibration {
+        Vec3 radialDistortion;
+        Vec2 tangentialDistortion;
+
+        Vec2 focalLength;
+        Vec2 opticalCenters;
+
+        /**
+         * @returns Mat3 representing the intrinsic matrix of the camera
+         */
+        inline Mat3 getMatrix() {
+            Mat3 m;
+            m << focalLength[0], 0, opticalCenters[0], 0, focalLength[1], opticalCenters[1], 0, 0, 1;
+            return m;
+        }
+    }
+}
